ee3ee6360074f2fc128ab1345d8e4cd7fa9b1088
Gregyyy <gregyyy@pm.me>
2021-04-09 00:23:34 +0200

    Redesign Challenger Card

--- src/components/ChallengerCard.vue
+++ src/components/ChallengerCard.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="card w-250" id="card">
-    <div id="headspace">
-      <img src="../assets/logo.png" :alt="player.username"
-           class="profilePicture centerVertically img-fluid w-25 h-25"
-           id="userIcon">
-      <h2 class="card-title centerVertically text-truncate">{{ player.username }}</h2>
-    </div>
+  <div class="card w-200 p-0" id="card">
+    <img src="../assets/person.jpg" :alt="player.username"
+         class="img-fluid rounded-top"
+         id="userIcon">
 
-    <div id="content">
-      <div class="icon-text-pair" style="float: left;">
-        <div class="material-icons-outlined centerVertically">emoji_events</div>
-        {{ player.wins }}
-      </div>
-
-      <div class="icon-text-pair">
-        <div class="material-icons-outlined centerVertically">view_list</div>
-        {{ player.reputationPoints }}
+    <div class="content" id="content">
+      <h2 class="card-title text-truncate">{{ player.username }}</h2>
+      <div>
+        <span class="badge">
+          <div class="material-icons-outlined align-middle font-size-18 mr-5" style="margin-top: auto">emoji_events</div>
+          <span class="align-middle align-text-bottom">{{ player.wins }}</span>
+        </span>
+        <span class="badge ml-5">
+          <div class="material-icons-outlined align-middle font-size-18 mr-5">view_list</div>
+          <span class="align-middle align-text-bottom">{{ player.reputationPoints }}</span>
+        </span>
       </div>
     </div>
   </div>
