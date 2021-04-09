698e1122b5ebf198f23638fcabe267643325bd21
Niklas @yDev <niklas@magic-computer.de>
2021-04-09 12:41:45 +0200

    Remove unused method and styles.

--- src/components/ChallengerCard.vue
+++ src/components/ChallengerCard.vue
@@ -27,15 +27,6 @@ export default {
   props: {
     player: Player
   },
-
-  methods: {
-    nameWithAdjustedLength(value) {
-      if (value.length > 12) {
-        return value.substring(0, 12) + "...";
-      }
-      return value;
-    }
-  },
 }
 </script>
 
@@ -46,34 +37,7 @@ export default {
   margin-right: 0;
 }
 
-#headspace {
-  position: relative;
-  height: 50px;
-}
-#headspace h2 {
-  font-size: 1.7rem;
-  text-align: left;
-}
-
 #content {
   font-family: 'Noto Sans JP', sans-serif;
-}
-
-.centerVertically {
-  position: relative;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-.profilePicture {
-  float: left;
-  border: 2px solid #807d7d;
-  border-radius: 100%;
-  margin-right: 12px;
-}
-
-.icon-text-pair {
-  position: relative;
-  height: 32px;
 }
 </style>
