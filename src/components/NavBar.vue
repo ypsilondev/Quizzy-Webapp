d95d964c50493b287cbba306e47ceb5ec4522bed
Gregyyy <gregyyy@pm.me>
2021-04-15 13:42:50 +0200

    Add profile image and dropdown to navbar. Move toggle darkt heme button.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,20 +2,35 @@
   <!-- Navbar (immediate child of the page wrapper) -->
   <nav class="navbar">
     <!-- Navbar brand -->
-    <a href="/" class="navbar-brand">
-      <img src="../assets/logo.png" alt="">
+    <router-link to="/" class="navbar-brand">
+      <img src="../assets/logo.png" alt="Quizzy Brand">
       {{ appName }}
-    </a>
+    </router-link>
     <!-- Navbar nav -->
     <ul class="navbar-nav d-none d-md-flex"> <!-- d-none = display: none, d-md-flex = display: flex on medium screens and up (width > 768px) -->
       <li class="nav-item active">
-        <a href="#" class="nav-link">Docs</a>
+        <a href="/" class="nav-link">Home</a>
       </li>
     </ul>
 
-    <button class="btn btn-action ml-auto" @click="halfmoon.toggleDarkMode()">
-      <span class="material-icons-outlined">dark_mode</span>
-    </button>
+    <div class="navbar-content ml-auto d-none d-md-flex" style="height: 100%">
+      <div class="dropdown" style="height: 100%">
+        <a href="#" data-toggle="dropdown" id="avatar-popover-toggle" aria-haspopup="true" aria-expanded="false"
+           style="height: 100%; display: flex; align-items: center">
+          <img src="../assets/person.jpg" class="img-fluid rounded-circle" style="height: 66%;" alt="Your profil">
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="avatar-popover-toggle">
+          <h6 class="dropdown-header">{{ user.displayName }}</h6>
+          <div class="dropdown-divider"></div>
+          <router-link class="dropdown-item" to="/profile">Profil</router-link>
+          <router-link class="dropdown-item" to="/logout">Logout</router-link>
+          <div class="dropdown-divider"></div>
+          <div class="dropdown-content">
+            <button class="btn btn-block" @click="halfmoon.toggleDarkMode()">Dunkelmodus umschalten</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- Navbar content (with the dropdown menu) -->
     <div class="navbar-content d-md-none ml-auto"> <!-- d-md-none = display: none on medium screens and up (width > 768px), ml-auto = margin-left: auto -->
@@ -25,9 +40,16 @@
           <i class="fa fa-angle-down" aria-hidden="true"></i>
         </button>
         <div class="dropdown-menu dropdown-menu-right w-200" aria-labelledby="navbar-dropdown-toggle-btn-1"> <!-- w-200 = width: 20rem (200px) -->
-          <a href="#" class="dropdown-item">Docs</a>
-          <a href="#" class="dropdown-item">Products</a>
+          <a href="#" class="dropdown-item">Home</a>
           <div class="dropdown-divider"></div>
+          <h6 class="dropdown-header">{{ user.displayName }}</h6>
+          <div class="dropdown-divider"></div>
+          <router-link class="dropdown-item" to="/profile">Profil</router-link>
+          <router-link class="dropdown-item" to="/logout">Logout</router-link>
+          <div class="dropdown-divider"></div>
+          <div class="dropdown-content">
+            <button class="btn btn-block" @click="halfmoon.toggleDarkMode()">Dunkelmodus umschalten</button>
+          </div>
         </div>
       </div>
     </div>
@@ -41,13 +63,20 @@ import Util from "@/Util.ts";
 export default {
   setup() {
     return {
-      halfmoon,
+      halfmoon
     }
   },
 
   data() {
     return {
-      appName: Util.APP_NAME
+      appName: Util.APP_NAME,
+      //TODO: Use real user
+      user: {
+        displayName: "Username",
+        _id: "_id",
+        profileImage: "606f801770402029ae887153",
+        totalScore: 1337
+      }
     }
   }
 }
