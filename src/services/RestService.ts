a227f04102307b3ca921b6e2f1d282466c399060
Gregyyy <gregyyy@pm.me>
2021-04-17 18:35:41 +0200

    Fix some things pointed out in the code review

--- src/services/RestService.ts
+++ src/services/RestService.ts
@@ -1,22 +1,20 @@
-import {User} from "@/types/User";
-
 export class RestService {
 
     private static API_HOST = "https://quiz.shirkanesi.com"
 
-    private static sendRequest(url: string, methode: string, data: object) {
+    private static sendRequest(url: string, method: string, data: object) {
         if (Object.keys(data).length > 0) {
             return fetch(this.API_HOST + url, {
-                method: methode,
+                method: method,
                 credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data)
             });
-        }else{
+        } else {
             return fetch(this.API_HOST + url, {
-                method: methode,
+                method: method,
                 credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json'
@@ -31,31 +29,25 @@ export class RestService {
      * @param password  Password of user
      * @return User     User object or null if login was not successful
      */
-    static async loginUser(username: string, password: string) {
+    static async loginUser(username: string, password: string) : Promise<User | null> {
         const response = await this.sendRequest("/users/authenticate/", "POST", {
             loginName: username,
             password: password
         });
-        if(response.status == 200){
+        if (response.status == 200) {
             const json = await response.json();
-            const user: User = json.user;
-            return user;
-        }else if(response.status == 400){
-            return null;
+            return json.user;
         }
+        return null;
     }
 
     /**
      * Checks whether the user is logged in
      * @return boolean  If action was successful
      */
-    static async isLogin() {
+    static async isLogin() : Promise<Boolean> {
         const response = await this.sendRequest("/users/logincheck/", "GET", {});
-        if (response.status == 200) {
-            return true;
-        } else if (response.status == 400) {
-            return false;
-        }
+        return response.status == 200;
     }
 
     /**
@@ -66,49 +58,36 @@ export class RestService {
      * @param profileImage  Id of profile image
      * @return User         User object
      */
-    static async registerUser(username: string, email: string, password: string, profileImage: string) {
+    static async registerUser(username: string, email: string, password: string, profileImage: string) : Promise<User | null> {
         const response = await this.sendRequest("/users/register/", "POST", {
             displayName: username,
             email: email,
             password: password,
             profileImage: profileImage
         });
-        if(response.status == 200){
-            response.json().then(json => {
-                const user: User = json;
-                return user;
-            })
-        }else if(response.status == 400){
-            return null;
+        if (response.status == 200) {
+            const json = await response.json();
+            return json.user;
         }
+        return null;
     }
 
     /**
      * Revokes all current session-tokens of the user
      * @return boolean  If action was successful
      */
-    static async revokeAllTokens() {
+    static async revokeAllTokens() : Promise<Boolean> {
         const response = await this.sendRequest("/users/security/revokeAllTokens/", "POST", {});
-        if(response.status == 200){
-            return true;
-        }else if(response.status == 400){
-            //TODO: maybe more detailed errors
-            return false;
-        }
+        return response.status == 200;
     }
 
     /**
      * Revokes the current session-token of the user [in the current browser]
      * @return boolean  If action was successful
      */
-    static async revokeToken() {
+    static async revokeToken() : Promise<Boolean> {
         const response = await this.sendRequest("/users/security/revokeToken/", "POST", {});
-        if(response.status == 200){
-            return true;
-        }else if(response.status == 400){
-            //TODO: maybe more detailed errors
-            return false;
-        }
+        return response.status == 200;
     }
 
     /**
@@ -116,14 +95,9 @@ export class RestService {
      * @param verificationNumber    The 6-digit number the user received via e-mail
      * @return boolean              If verification was successful
      */
-    static async verifyEMail(verificationNumber: number) {
+    static async verifyEMail(verificationNumber: number) : Promise<Boolean> {
         const response = await this.sendRequest("/users/security/verify/" + verificationNumber, "GET", {});
-        if(response.status == 200){
-            return true;
-        }else if(response.status == 400){
-            //TODO: maybe more detailed errors
-            return false;
-        }
+        return response.status == 200;
     }
 
 }
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,13 +1,6 @@
-export class User {
+interface User {
     displayName: String;
     _id: String;
     profileImage: String;
     totalScore: number;
-
-    constructor(displayName: String, id: String, profileImage: String, totalScore: number) {
-        this.displayName = displayName;
-        this._id = id;
-        this.profileImage = profileImage;
-        this.totalScore = totalScore;
-    }
 }
